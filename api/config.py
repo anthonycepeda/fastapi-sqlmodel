7faf84ad30539fe7c10847545765d28925563143
anthonycepeda <cepedanthony@gmail.com>
2022-12-17 17:03:31 +0100

    fix: missing () on creating dbs on startup

--- api/config.py
+++ api/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = f"SQLModel API - {os.getenv('ENV', 'development').capitalize()}"
     DESCRIPTION: str = "A FastAPI + SQLModel production-ready API"
     ENV: str
-    VERSION: str
+    VERSION: str = "0.1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # DATABASE_URI: Optional[PostgresDsn] = None
     DATABASE_URI: Optional[str]  # sqlite tmp db just for test
